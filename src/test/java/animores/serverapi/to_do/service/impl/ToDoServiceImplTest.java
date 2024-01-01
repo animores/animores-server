0c148c726f1fa53752741392ddbe464becbc5346
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2024-01-01 17:49:29 +0900

    feat: 기본적인 API 틀 추가

---
+++ src/test/java/animores/serverapi/to_do/service/impl/ToDoServiceImplTest.java
@@ -0,0 +1,37 @@
+package animores.serverapi.to_do.service.impl;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ToDoServiceImplTest {
+
+	@InjectMocks
+	private ToDoServiceImpl toDoServiceImplUnderTest;
+
+	@Test
+	void createToDo() {
+	}
+
+	@Test
+	void getTodayToDo() {
+	}
+
+	@Test
+	void getAllToDo() {
+	}
+
+	@Test
+	void getToDoById() {
+	}
+
+	@Test
+	void updateToDoById() {
+	}
+
+	@Test
+	void deleteToDoById() {
+	}
+}
