440e1e36cc8e1cf6470d545dc31b07c0bb06228c
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2024-01-01 18:28:44 +0900

    feat : ToDoRepository 추가

---
+++ src/main/java/animores/serverapi/to_do/repository/ToDoRepository.java
@@ -0,0 +1,9 @@
+package animores.serverapi.to_do.repository;
+
+import animores.serverapi.to_do.ToDo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ToDoRepository extends JpaRepository<ToDo, Long> {
+}
