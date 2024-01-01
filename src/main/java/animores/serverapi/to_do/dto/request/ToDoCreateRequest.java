c24020f4464b4985ba1eb44c3d06a5194869610d
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2024-01-01 18:25:33 +0900

    feat : request, response parameter 입력

--- src/main/java/animores/serverapi/to_do/dto/request/ToDoCreateRequest.java
+++ src/main/java/animores/serverapi/to_do/dto/request/ToDoCreateRequest.java
@@ -1,4 +1,19 @@
 package animores.serverapi.to_do.dto.request;
 
-public record ToDoCreateRequest() {
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+public record ToDoCreateRequest(
+		List<Long> petIds,
+		Boolean isCustom,
+		String title,
+		LocalDate date,
+		LocalTime time,
+		Boolean isAllDay,
+		String color,
+		Boolean hasAlarm
+		// TODO: 반복 관련 부분 추가
+) {
+
 }
