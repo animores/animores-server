22d949b28fb3064cae0c43f1ddedf275713ee831
kangmo <22rkdmin@naver.com>
2024-01-20 01:15:53 +0900

    fix: validation 안되던 거 수정(#16)
    hibernate-validator 의존성 추가

--- src/main/java/animores/serverapi/account/controller/AccountController.java
+++ src/main/java/animores/serverapi/account/controller/AccountController.java
@@ -1,20 +1,19 @@
 package animores.serverapi.account.controller;
 
+
 import animores.serverapi.account.request.AccountCreateRequest;
 import animores.serverapi.account.response.AccountCreateResponse;
 import animores.serverapi.account.service.AccountService;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/account")
-@Validated
 public class AccountController {
 
     private final AccountService accountService;
@@ -36,7 +35,7 @@ public class AccountController {
     }
 
     @GetMapping("/check-nickname/{nickname}")
-    public ResponseEntity<Boolean> isDuplicatedNickname(@PathVariable @NotBlank String nickname) {
+    public ResponseEntity<Boolean> isDuplicatedNickname(@PathVariable @Size(min = 2, max = 8) String nickname) {
         return ResponseEntity.ok().body(accountService.isDuplicatedNickname(nickname));
     }
 
