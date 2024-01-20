44db3269e9d13451efb1698629cd81927dfa6dc0
kangmo <22rkdmin@naver.com>
2024-01-20 13:17:35 +0900

    fix: dto validation 수정(#16)

--- src/main/java/animores/serverapi/account/request/AccountCreateRequest.java
+++ src/main/java/animores/serverapi/account/request/AccountCreateRequest.java
@@ -2,6 +2,7 @@ package animores.serverapi.account.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record AccountCreateRequest (
@@ -13,14 +14,14 @@ public record AccountCreateRequest (
                 message = "8~30자리 영대소문자, 숫자, 특수문자 조합이어야 합니다.")
         String password,
 
-        @NotEmpty
+        @NotEmpty(message = "비밀번호 확인을 입력 하세요.")
         String confirmPassword,
 
         @NotEmpty(message = "닉네임을 입력 하세요.")
         String nickname,
 
-        @NotEmpty(message = "광고 수신 여부를 입력 하세요")
-        boolean ad_yn
+        @NotNull(message = "광고 수신 여부를 입력 하세요.")
+        Boolean ad_yn
 
 ) {
 }
