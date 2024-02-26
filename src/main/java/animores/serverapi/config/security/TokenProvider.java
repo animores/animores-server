23f048096f88c313e7acec1ccf352c7f5fa38e44
kangmo <22rkdmin@naver.com>
2024-02-26 14:49:22 +0900

    feat: @Value path 재설정(#25)

--- src/main/java/animores/serverapi/config/security/TokenProvider.java
+++ src/main/java/animores/serverapi/config/security/TokenProvider.java
@@ -3,6 +3,7 @@ package animores.serverapi.config.security;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.spec.SecretKeySpec;
@@ -18,9 +19,9 @@ public class TokenProvider {
     private final long expirationHours;
     private final String issuer;
 
-    public TokenProvider(@Value("${secret-key}") String secretKey,
-                         @Value("${expiration-hours}") long expirationHours,
-                         @Value("${issuer}") String issuer) {
+    public TokenProvider(@Value("${spring.jwt.secret-key}") String secretKey,
+                         @Value("${spring.jwt.expiration-hours}") long expirationHours,
+                         @Value("${spring.jwt.issuer}") String issuer) {
         this.secretKey = secretKey;
         this.expirationHours = expirationHours;
         this.issuer = issuer;
@@ -28,11 +29,11 @@ public class TokenProvider {
 
     public String createToken(String userSpecification) {
         return Jwts.builder()
-                .signWith(new SecretKeySpec(secretKey.getBytes(), SignatureAlgorithm.HS512.getJcaName()))   // HS512 알고리즘을 사용하여 secretKey를 이용해 서명
-                .setSubject(userSpecification)  // JWT 토큰 제목
-                .setIssuer(issuer)  // JWT 토큰 발급자
-                .setIssuedAt(Timestamp.valueOf(LocalDateTime.now()))    // JWT 토큰 발급 시간
-                .setExpiration(Date.from(Instant.now().plus(expirationHours, ChronoUnit.HOURS)))    // JWT 토큰 만료 시간
+                .signWith(new SecretKeySpec(secretKey.getBytes(), SignatureAlgorithm.HS512.getJcaName())) // HS512 알고리즘을 사용하여 secretKey를 이용해 서명
+                .setSubject(userSpecification) // JWT 토큰 제목
+                .setIssuer(issuer) // JWT 토큰 발급자
+                .setIssuedAt(Timestamp.valueOf(LocalDateTime.now())) // JWT 토큰 발급 시간
+                .setExpiration(Date.from(Instant.now().plus(expirationHours, ChronoUnit.HOURS))) // JWT 토큰 만료 시간
                 .compact(); // JWT 토큰 생성
     }
 }
