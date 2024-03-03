353017683f25c6ed838ae73b96a92b0e8577f48a
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2024-03-03 14:58:47 +0900

    feat: 기존 controller 변경

--- src/main/java/animores/serverapi/account/controller/AccountController.java
+++ src/main/java/animores/serverapi/account/controller/AccountController.java
@@ -4,11 +4,13 @@ package animores.serverapi.account.controller;
 import animores.serverapi.account.request.AccountCreateRequest;
 import animores.serverapi.account.response.AccountCreateResponse;
 import animores.serverapi.account.service.AccountService;
+import animores.serverapi.common.Response;
+import animores.serverapi.common.exception.CustomException;
+import animores.serverapi.common.exception.ExceptionCode;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,24 +21,25 @@ public class AccountController {
     private final AccountService accountService;
 
     @PostMapping
-    public ResponseEntity<AccountCreateResponse> createAccount(@Valid @RequestBody AccountCreateRequest request) throws Exception {
+    public Response<AccountCreateResponse> createAccount(@Valid @RequestBody AccountCreateRequest request) throws Exception {
         AccountCreateResponse response = accountService.createAccount(request);
 
         if (response == null) {
-            throw new Exception();
+            //TODO: 에러코드 추가
+            throw new CustomException(ExceptionCode.UNHANDLED_EXCEPTION);
         }
 
-        return ResponseEntity.ok().body(response);
+        return Response.success(response);
     }
 
     @GetMapping("/check-email/{email}")
-    public ResponseEntity<Boolean> isDuplicatedEmail(@PathVariable @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,6}$") String email) {
-        return ResponseEntity.ok().body(accountService.isDuplicatedEmail(email));
+    public Response<Boolean> isDuplicatedEmail(@PathVariable @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,6}$") String email) {
+        return Response.success(accountService.isDuplicatedEmail(email));
     }
 
     @GetMapping("/check-nickname/{nickname}")
-    public ResponseEntity<Boolean> isDuplicatedNickname(@PathVariable @Size(min = 2, max = 8) String nickname) {
-        return ResponseEntity.ok().body(accountService.isDuplicatedNickname(nickname));
+    public Response<Boolean> isDuplicatedNickname(@PathVariable @Size(min = 2, max = 8) String nickname) {
+        return Response.success(accountService.isDuplicatedNickname(nickname));
     }
 
 }
