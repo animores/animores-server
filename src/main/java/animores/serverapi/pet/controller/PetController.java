689197d97c9e0230afbdabf6ae27b93514641e19
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2024-04-28 15:25:43 +0900

    feat: 기본 object 추가

---
+++ src/main/java/animores/serverapi/pet/controller/PetController.java
@@ -0,0 +1,11 @@
+package animores.serverapi.pet.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/pet")
+public class PetController {
+
+
+}
