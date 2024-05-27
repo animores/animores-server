a20450529f69930a05fc86c2cec624f6fe14413d
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2024-05-27 22:23:08 +0900

    feat: 일반적인 상황에 적용하도록 urls 를 인수로 받도록 변경

--- src/main/java/animores/serverapi/common/service/S3Service.java
+++ src/main/java/animores/serverapi/common/service/S3Service.java
@@ -1,10 +1,10 @@
 package animores.serverapi.common.service;
 
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
 import java.io.IOException;
 import java.util.List;
-import org.springframework.web.multipart.MultipartFile;
-import software.amazon.awssdk.services.s3.model.ObjectIdentifier;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 public interface S3Service {
 
@@ -13,6 +13,6 @@ public interface S3Service {
 
     PutObjectRequest uploadFileToS3(MultipartFile file, String path) throws IOException;
 
-    void removeFilesFromS3(List<ObjectIdentifier> keys);
+    void removeFilesFromS3(List<String> urls);
 
 }
--- src/main/java/animores/serverapi/diary/service/impl/DiaryServiceImpl.java
+++ src/main/java/animores/serverapi/diary/service/impl/DiaryServiceImpl.java
@@ -1,7 +1,5 @@
 package animores.serverapi.diary.service.impl;
 
-import static animores.serverapi.common.S3Path.DIARY_PATH;
-
 import animores.serverapi.account.domain.Account;
 import animores.serverapi.common.exception.CustomException;
 import animores.serverapi.common.exception.ExceptionCode;
@@ -9,40 +7,29 @@ import animores.serverapi.common.service.AuthorizationService;
 import animores.serverapi.common.service.S3Service;
 import animores.serverapi.diary.dao.GetAllDiaryDao;
 import animores.serverapi.diary.dao.GetCalendarDiaryDao;
-import animores.serverapi.diary.dto.AddDiaryLikeRequest;
-import animores.serverapi.diary.dto.AddDiaryMediaRequest;
-import animores.serverapi.diary.dto.AddDiaryRequest;
-import animores.serverapi.diary.dto.CancelDiaryLikeRequest;
-import animores.serverapi.diary.dto.EditDiaryContentRequest;
-import animores.serverapi.diary.dto.EditDiaryMediaRequest;
-import animores.serverapi.diary.dto.GetAllDiaryResponse;
-import animores.serverapi.diary.dto.GetCalendarDiaryResponse;
-import animores.serverapi.diary.dto.RemoveDiaryRequest;
+import animores.serverapi.diary.dto.*;
 import animores.serverapi.diary.entity.Diary;
 import animores.serverapi.diary.entity.DiaryLike;
 import animores.serverapi.diary.entity.DiaryMedia;
 import animores.serverapi.diary.entity.DiaryMediaType;
-import animores.serverapi.diary.repository.DiaryCustomRepository;
-import animores.serverapi.diary.repository.DiaryLikeRepository;
-import animores.serverapi.diary.repository.DiaryMediaCustomRepository;
-import animores.serverapi.diary.repository.DiaryMediaRepository;
-import animores.serverapi.diary.repository.DiaryRepository;
+import animores.serverapi.diary.repository.*;
 import animores.serverapi.diary.service.DiaryService;
 import animores.serverapi.profile.domain.Profile;
 import animores.serverapi.profile.repository.ProfileRepository;
 import com.querydsl.core.QueryResults;
-import java.io.IOException;
-import java.time.LocalDate;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
-import software.amazon.awssdk.services.s3.model.ObjectIdentifier;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.IntStream;
+
+import static animores.serverapi.common.S3Path.DIARY_PATH;
 
 @Service
 @RequiredArgsConstructor
@@ -147,7 +134,11 @@ public class DiaryServiceImpl implements DiaryService {
         if (mediaListToDelete.isEmpty()) {
             throw new CustomException(ExceptionCode.NOT_FOUND_DIARY_MEDIA);
         }
-        s3Service.removeFilesFromS3(generateKeysForS3Deletion(mediaListToDelete));
+        s3Service.removeFilesFromS3(
+                mediaListToDelete.stream()
+                        .map(DiaryMedia::getUrl)
+                        .toList()
+        );
         diaryMediaRepository.deleteAll(mediaListToDelete);
 
         List<PutObjectRequest> putObjectRequests = s3Service.uploadFilesToS3(files, DIARY_PATH);
@@ -169,7 +160,11 @@ public class DiaryServiceImpl implements DiaryService {
         if (mediaListToDelete.isEmpty()) {
             throw new CustomException(ExceptionCode.NOT_FOUND_DIARY_MEDIA);
         }
-        s3Service.removeFilesFromS3(generateKeysForS3Deletion(mediaListToDelete));
+        s3Service.removeFilesFromS3(
+                mediaListToDelete.stream()
+                        .map(DiaryMedia::getUrl)
+                        .toList()
+        );
         diaryMediaRepository.deleteAll(mediaListToDelete);
 
         reorderDiaryMedia(diary.getId(), DiaryMediaType.I);
@@ -226,11 +221,9 @@ public class DiaryServiceImpl implements DiaryService {
     private List<DiaryMedia> createDiaryMedias(Diary diary,
         List<PutObjectRequest> putObjectRequests) {
         return IntStream.range(0, putObjectRequests.size())
-            .mapToObj(i -> {
-                return DiaryMedia.create(diary, putObjectRequests.get(i).key(), i,
-                    DiaryMediaType.checkType(putObjectRequests.get(i).contentType()));
-            })
-            .collect(Collectors.toList());
+            .mapToObj(i -> DiaryMedia.create(diary, putObjectRequests.get(i).key(), i,
+                DiaryMediaType.checkType(putObjectRequests.get(i).contentType())))
+                .toList();
     }
 
     public void reorderDiaryMedia(Long diaryId, DiaryMediaType type) {
@@ -240,13 +233,5 @@ public class DiaryServiceImpl implements DiaryService {
         for (int i = 0; i < mediaList.size(); i++) {
             mediaList.get(i).updateMediaOrder(i);
         }
-    }
-
-    private List<ObjectIdentifier> generateKeysForS3Deletion(List<DiaryMedia> mediaList) {
-        return mediaList.stream()
-            .map(media -> ObjectIdentifier.builder()
-                .key(media.getUrl())
-                .build())
-            .collect(Collectors.toList());
     }
 }
