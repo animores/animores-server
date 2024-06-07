5ac42083001c2d2d6c282004e5b7f42dc5367412
sunveloper <96734777+seongyeong826@users.noreply.github.com>
2024-06-07 23:37:12 +0900

    refactor: AWS 자격증명, S3 config 분리 (#92)

--- src/main/java/animores/serverapi/config/S3Config.java
+++ src/main/java/animores/serverapi/config/S3Config.java
@@ -3,41 +3,21 @@ package animores.serverapi.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3Config {
 
-    @Value("${spring.cloud.aws.credentials.accessKey}")
-    private String accessKey;
-
-    @Value("${spring.cloud.aws.credentials.secretKey}")
-    private String accessSecret;
-
     @Value("${spring.cloud.aws.region.static}")
     private String region;
 
     @Bean
-    public S3Client s3Client() {
+    public S3Client s3Client(AwsCredentialsProvider awsCredentialsProvider) {
         return S3Client.builder()
-            .credentialsProvider(this::awsCredentials)
+            .credentialsProvider(awsCredentialsProvider)
             .region(Region.of(region))
             .build();
-    }
-
-    private AwsCredentials awsCredentials() {
-        return new AwsCredentials() {
-            @Override
-            public String accessKeyId() {
-                return accessKey;
-            }
-
-            @Override
-            public String secretAccessKey() {
-                return accessSecret;
-            }
-        };
     }
 }
