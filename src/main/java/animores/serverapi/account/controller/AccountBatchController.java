62d0dfd763922cc41b9f25c4723426328e6e5738
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2024-06-13 16:45:44 +0900

    document: swagger 설명 추가

--- src/main/java/animores/serverapi/account/controller/AccountBatchController.java
+++ src/main/java/animores/serverapi/account/controller/AccountBatchController.java
@@ -2,6 +2,8 @@ package animores.serverapi.account.controller;
 
 import animores.serverapi.account.service.AccountBatchService;
 import animores.serverapi.common.Response;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +18,10 @@ public class AccountBatchController {
     private final AccountBatchService accountBatchService;
 
     @PostMapping("/")
-    public Response<Void> insertAccountBatch(@RequestParam Integer count) {
+    @Operation(summary = "계정 배치 생성", description = "계정을 배치로 생성합니다.")
+    public Response<Void> insertAccountBatch(@RequestParam
+                                             @Parameter(description = "배치 개수", required = true, example = "100")
+                                             Integer count) {
         accountBatchService.insertAccountBatch(count);
         return Response.success(null);
     }
--- src/main/java/animores/serverapi/diary/controller/DiaryBatchController.java
+++ src/main/java/animores/serverapi/diary/controller/DiaryBatchController.java
@@ -2,6 +2,8 @@ package animores.serverapi.diary.controller;
 
 import animores.serverapi.common.Response;
 import animores.serverapi.diary.service.DiaryBatchService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,20 +17,38 @@ public class DiaryBatchController {
 
     private final DiaryBatchService diaryBatchService;
 
+    @Operation(summary = "일지 배치 생성", description = "일지를 배치로 생성합니다.")
     @PostMapping("/")
-    public Response<Void> insertDiaryBatch(@RequestParam Integer count, @RequestParam Long accountId) {
+    public Response<Void> insertDiaryBatch(@RequestParam
+                                           @Parameter(description = "배치 개수", required = true, example = "100")
+                                           Integer count,
+                                           @RequestParam
+                                           @Parameter(description = "일지를 생성할 계정 id", required = true, example = "1")
+                                           Long accountId) {
         diaryBatchService.insertDiaryBatch(count, accountId);
         return Response.success(null);
     }
 
+    @Operation(summary = "일지 댓글 배치 생성", description = "일지 댓글을 배치로 생성합니다.")
     @PostMapping("/comment")
-    public Response<Void> insertDiaryCommentBatch(@RequestParam Integer count, @RequestParam Long diaryId) {
+    public Response<Void> insertDiaryCommentBatch(@RequestParam
+                                                  @Parameter(description = "배치 개수", required = true, example = "100")
+                                                  Integer count,
+                                                  @RequestParam
+                                                  @Parameter(description = "일지 id", required = true, example = "1")
+                                                  Long diaryId) {
         diaryBatchService.insertDiaryCommentBatch(count, diaryId);
         return Response.success(null);
     }
 
+    @Operation(summary = "일지 좋아요 배치 생성", description = "일지 좋아요를 배치로 생성합니다. 입력 받은 계정 id 의 diary 들에 계정의 profile 들이 좋아요를 남깁니다.")
     @PostMapping("/like")
-    public Response<Void> insertDiaryLikeBatch(@RequestParam Integer count, @RequestParam Long accountId) {
+    public Response<Void> insertDiaryLikeBatch(@RequestParam
+                                                   @Parameter(description = "배치 개수", required = true, example = "100")
+                                                   Integer count,
+                                               @RequestParam
+                                                   @Parameter(description = "계정 id", required = true, example = "1")
+                                                   Long accountId) {
         diaryBatchService.insertDiaryLikeBatch(count, accountId);
         return Response.success(null);
     }
