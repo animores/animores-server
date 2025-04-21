d28a311ed79656e3926ff323dc7fa14ea9dd25b4
sunveloper <96734777+seongyeong826@users.noreply.github.com>
2025-04-22 00:29:37 +0900

    hotfix: commentId가 아닌 profileId를 전달하는 문제 해결 (#117)

--- src/main/java/animores/serverapi/diary/repository/DiaryCommentCustomRepository.java
+++ src/main/java/animores/serverapi/diary/repository/DiaryCommentCustomRepository.java
@@ -8,8 +8,7 @@ public interface DiaryCommentCustomRepository {
 
     List<GetAllDiaryCommentDao> getAllDiaryComment(Long diaryId, int page, int size);
 
-    List<GetAllDiaryReplyDao> getAllDiaryReply(Long commentId, int page,
-        int size);
+    List<GetAllDiaryReplyDao> getAllDiaryReply(Long commentId, int page, int size);
 
     Long getAllDiaryCommentCount(Long diaryId);
 }
